ae1daf6f2c51a5ddc68835b5c8284867ecd33f1e
Thomas Martin <thomas.martin@nwea.org>
2022-11-21 10:53:50 -0500

    test

--- lib/facter/alert_manager_running.rb
+++ lib/facter/alert_manager_running.rb
@@ -4,9 +4,11 @@ require 'puppet'
 
 Facter.add('prometheus_alert_manager_running') do
   setcode do
-    service = Puppet::Type.type(:service).new(:name => 'alert_manager') # rubocop:disable Style/HashSyntax
-    service.provider.status == :running
-  rescue Puppet::Error
-    false
+    begin
+      service = Puppet::Type.type(:service).new(:name => 'alert_manager') # rubocop:disable Style/HashSyntax
+      service.provider.status == :running
+    rescue Puppet::Error
+      false
+    end
   end
 end
